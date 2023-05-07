eda56c57ef593e0ff5307d957e754aec924f2584
gitluthfi <112075939+gitluthfi@users.noreply.github.com>
2023-05-07 23:25:59 +0700

    Update kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-
+#!/bin/bash
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
